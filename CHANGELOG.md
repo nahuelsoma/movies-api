58dbcb54a4f030bf90f003e22f662cfe883252e9
Nahuel Soma <soma.nahuel@gmail.com>
2023-10-06 16:43:02 -0300

    Add movie creation endpoint (#2)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,4 +4,5 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+- Add movie creation endpoint ([#2](https://github.com/nahuelsoma/movies-api/pull/2))
 - Add Prisma, Docker, PostgreSQL and seed data endpoint ([#1](https://github.com/nahuelsoma/movies-api/pull/1))
--- src/modules/movies/index.ts
+++ src/modules/movies/index.ts
@@ -3,16 +3,11 @@ import { MoviesController } from 'src/controllers/movies';
 import { MoviesService } from 'src/services/movies';
 import { StarwarsRepository } from 'src/repositories/starwars';
 import { MoviesRepository } from 'src/repositories/movies';
-import { DatabaseRepository } from 'src/repositories/database';
+import { Database } from 'src/infrastructure/database';
 
 @Module({
   imports: [],
   controllers: [MoviesController],
-  providers: [
-    DatabaseRepository,
-    MoviesRepository,
-    MoviesService,
-    StarwarsRepository,
-  ],
+  providers: [Database, MoviesRepository, MoviesService, StarwarsRepository],
 })
 export class MoviesModule {}
