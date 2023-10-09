93cfdaa79a3bf436d054dde85e336c226ea5ffa4
Nahuel Soma <soma.nahuel@gmail.com>
2023-10-09 12:15:58 -0300

    [EDIT] repositories logs

--- src/repositories/starwars/index.ts
+++ src/repositories/starwars/index.ts
@@ -1,27 +1,27 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 import axios, { AxiosError } from 'axios';
 
 export class StarwarsRepository {
+  constructor(private readonly logger: Logger) {}
+
   async getSeedData(): Promise<StarwarsMovies> {
     try {
       const { data } = await axios<StarwarsMovies>({
         method: 'get',
-        url: 'https://swapi.dev/api/films',
+        url: 'https://swapi.dev/api/filmsa',
       });
 
       return data;
     } catch (error) {
-      // enhance error logging
-      console.log('error in StarwarsRepository.seedData: ', error.message);
-
       if (error instanceof AxiosError) {
         throw error;
       }
 
-      throw new InternalServerErrorException(
-        'Error getting movies from starwars api',
-        error.message,
-      );
+      const message = 'Error getting movies from starwars api';
+
+      this.logger.error(message, error.stack, 'InternalServerErrorException');
+
+      throw new InternalServerErrorException(message, error.message);
     }
   }
 }
